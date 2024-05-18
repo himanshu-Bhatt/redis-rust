2bd751d1b77e00d970c32251be572d04b562bfd4
Himanshu Bhatt <himanshubhatt@Himanshus-MacBook-Air.local>
2024-05-18 17:57:35 +0530

    Echo command

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 // Uncomment this block to pass the first stage
+// use resp::Value;
+// use resp::Value::Bulk;
 use std::{
+    env,
     io::{Read, Write},
-    net::TcpListener,
+    net::{TcpListener, TcpStream},
     thread,
 };
+
+use redis::Value;
+use redis::{ToRedisArgs};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -12,23 +18,12 @@ fn main() {
     // Uncomment this block to pass the first stage
     //
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-
     for stream in listener.incoming() {
         match stream {
             Ok(mut _stream) => {
                 println!("accepted new connection");
                 thread::spawn(move || {
-                    let mut buf = [0; 512];
-                    loop {
-                        let r_c = _stream.read(&mut buf).unwrap();
-                        if r_c == 0 {
-                            break;
-                        }
-                       
-                        _stream
-                            .write(b"+PONG\r\n")
-                            .expect("failed to write to stream");
-                    }
+                    handle_conn(&mut _stream);
                 });
             }
             Err(e) => {
@@ -36,4 +31,51 @@ fn main() {
             }
         }
     }
+}
+fn handle_conn(stream: &mut TcpStream) {
+    let mut temp_buf = [0; 512];
+    match stream.read(&mut temp_buf) {
+        Ok(_) => {
+            if let Ok(v) = redis::parse_redis_value(&temp_buf) {
+                let mut comms = Vec::new();
+                match v {
+                    Value::Bulk(ref items) => {
+                        for item in items {
+                            if let Value::Data(ref data) = item {
+                                let st = String::from_utf8_lossy(data);
+                                println!("{}", st);
+                                comms.push(st);
+                            } else {
+                                println!("Unexpected item in bulk response");
+                            }
+                        }
+                    }
+                    _ => println!("Unexpected response type"),
+                }
+                if comms[0] == "PING" {
+                    stream.write(b"+PONG\r\n");
+                } else if comms[0] == "ECHO" {
+                  let echo_this=  comms.iter().skip(1).map(|word|word.as_ref()).collect::<Vec<&str>>().join(" ");
+                    let resp_en=to_resp_string(echo_this);
+                    stream.write(resp_en.as_bytes());
+                }
+            } else {
+                println!("Error while parsing");
+            }
+        }
+        Err(e) => {
+            println!("{:?}", e);
+        }
+    }
+}
+fn to_resp_string<T: ToRedisArgs>(value: T) -> String {
+    let args = value.to_redis_args();
+    println!("{:?}",args);
+    let mut resp = String::new();
+    for arg in args {
+        resp.push_str(&format!("${}\r\n", arg.len()));
+        resp.push_str(&String::from_utf8_lossy(&arg));
+        resp.push_str("\r\n");
+    }
+    resp
 }
