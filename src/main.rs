f03ae72e54e19a9f03d1f935c644881c0fdc1c11
Himanshu Bhatt <himanshubhatt@Himanshus-MacBook-Air.local>
2024-05-28 01:43:10 +0530

    SET with expiry done

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,23 @@
-// Uncomment this block to pass the first stage
-// use resp::Value;
-// use resp::Value::Bulk;
+use redis::Value;
 use std::{
     collections::HashMap,
-    env,
     io::{Read, Write},
     net::{TcpListener, TcpStream},
     sync::{Arc, Mutex},
-    thread,
 };
+use tokio::time::{sleep, Duration};
 
-use redis::ToRedisArgs;
-use redis::Value;
-
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    // println!("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
-    //
+#[tokio::main]
+async fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    let mut map = Arc::new(Mutex::new(HashMap::new()));
+    let map = Arc::new(Mutex::new(HashMap::new()));
     for stream in listener.incoming() {
         match stream {
             Ok(mut _stream) => {
                 println!("accepted new connection");
                 let _map = Arc::clone(&map);
-                thread::spawn(move || {
-                    handle_conn(&mut _stream, _map);
+                tokio::spawn(async move {
+                    handle_conn(&mut _stream, _map).await;
                 });
             }
             Err(e) => {
@@ -36,7 +26,7 @@ fn main() {
         }
     }
 }
-fn handle_conn(stream: &mut TcpStream, map: Arc<Mutex<HashMap<String, String>>>) {
+async fn handle_conn(stream: &mut TcpStream, map: Arc<Mutex<HashMap<String, String>>>) {
     let mut temp_buf = [0; 512];
 
     loop {
@@ -51,7 +41,6 @@ fn handle_conn(stream: &mut TcpStream, map: Arc<Mutex<HashMap<String, String>>>)
                         for item in items {
                             if let Value::Data(ref data) = item {
                                 let st = String::from_utf8_lossy(data);
-                                // println!("{}", st);
                                 comms.push(st);
                             } else {
                                 println!("Unexpected item in bulk response");
@@ -61,31 +50,69 @@ fn handle_conn(stream: &mut TcpStream, map: Arc<Mutex<HashMap<String, String>>>)
                     _ => println!("Unexpected response type"),
                 }
 
-                if comms[0] == "PING" {
-                    stream.write(b"+PONG\r\n");
-                } else if comms[0] == "ECHO" {
-                    let echo_this = comms
-                        .iter()
-                        .skip(1)
-                        .map(|word| word.as_ref())
-                        .collect::<Vec<&str>>()
-                        .join(" ");
-                    let resp_en = to_resp_string(echo_this);
-                    stream.write(resp_en.as_bytes());
-                } else if comms[0] == "SET" {
-                    let k = comms[1].to_string();
-                    let v = comms[2].to_string();
-                    let mut map = map.lock().unwrap();
-                    map.insert(k, v);
-                    stream.write(b"+OK\r\n");
-                } else if comms[0] == "GET" {
-                     let map = map.lock().unwrap();
-                    let k = comms[1].as_ref();
-                    if let Some(res) = map.get(k) {
-                        let v = format!("${}\r\n{}\r\n", res.len(), res.to_string());
-                        stream.write(v.as_bytes());
-                    } else {
-                        stream.write(b"-1\r\n");
+                match &*comms[0] {
+                    "PING" => {
+                        if let Err(e) = stream.write(b"+PONG\r\n") {
+                            eprintln!("An error occurred while writing to stream: {}", e);
+                        }
+                    }
+                    "ECHO" => {
+                        let echo_this = comms
+                            .iter()
+                            .skip(1)
+                            .map(|word| word.as_ref())
+                            .collect::<Vec<&str>>()
+                            .join(" ");
+                        if let Err(e) = stream
+                            .write(format!("${}\r\n{}\r\n", echo_this.len(), echo_this).as_bytes())
+                        {
+                            eprintln!("An error occurred while writing to stream: {}", e);
+                        }
+                    }
+                    "SET" => {
+                        println!("Hello");
+                        if comms.len() == 3 {
+                            let k = comms[1].to_string();
+                            let v = comms[2].to_string();
+                            let mut map = map.lock().unwrap();
+                            map.insert(k, v);
+                            if let Err(e) = stream.write(b"+OK\r\n") {
+                                eprintln!("An error occurred while writing to stream: {}", e);
+                            }
+                        } else if comms.len() == 5 && comms[3] == "px" {
+                            let _tm = (&*comms[4]).to_string();
+                            let tm: u64 = _tm.parse().unwrap();
+                            let k = comms[1].to_string();
+                            let v = comms[2].to_string();
+                            {
+                                let mut _map = map.lock().unwrap();
+                                _map.insert(k, v);
+                            }
+                            if let Err(e) = stream.write(b"+OK\r\n") {
+                                eprintln!("An error occurred while writing to stream: {}", e);
+                            }
+
+                            let _map = Arc::clone(&map);
+                            let k = (&*comms[1]).to_string();
+                            delete_key(k, _map, tm).await;
+                        }
+                    }
+                    "GET" => {
+                        let map = map.lock().unwrap();
+                        let k = comms[1].as_ref();
+                        if let Some(res) = map.get(k) {
+                            let v = format!("${}\r\n{}\r\n", res.len(), res.to_string());
+                            if let Err(e) = stream.write(v.as_bytes()) {
+                                eprintln!("An error occurred while writing to stream: {}", e);
+                            }
+                        } else {
+                            if let Err(e) = stream.write(b"-1\r\n") {
+                                eprintln!("An error occurred while writing to stream: {}", e);
+                            }
+                        }
+                    }
+                    _ => {
+                        println!("Unknown command");
                     }
                 }
             } else {
@@ -96,14 +123,15 @@ fn handle_conn(stream: &mut TcpStream, map: Arc<Mutex<HashMap<String, String>>>)
         }
     }
 }
-fn to_resp_string<T: ToRedisArgs>(value: T) -> String {
-    let args = value.to_redis_args();
-    println!("{:?}", args);
-    let mut resp = String::new();
-    for arg in args {
-        resp.push_str(&format!("${}\r\n", arg.len()));
-        resp.push_str(&String::from_utf8_lossy(&arg));
-        resp.push_str("\r\n");
-    }
-    resp
+async fn delete_key(k: String, map: Arc<Mutex<HashMap<String, String>>>, ts: u64) {
+    println!("Hello");
+    sleep(Duration::from_millis(ts)).await;
+    let k = &k[..];
+
+    // Lock the mutex to access the HashMap
+    let mut map = map.lock().unwrap();
+
+    // Delete the entry
+    map.remove(k);
+    println!("Entry for key '{}' deleted", k);
 }
